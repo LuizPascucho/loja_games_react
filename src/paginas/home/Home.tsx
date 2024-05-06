59978e543f5110298c25fe4ea0deb0a0d6e9768c
Luiz Pascucho <luizpascucho@bol.com.br>
2024-05-06 15:18:42 -0300

    estilizando

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -11,7 +11,7 @@ function Home() {
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white bg-purple-950 py-2 px-4'>Veja nossos produtos</button>
+                <button className='rounded bg-white bg-purple-700 py-2 px-4'>Veja nossos produtos</button>
               </div>
             </div>
   
